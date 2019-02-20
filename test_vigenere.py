749d8344ff2eceddbc8d37b679e213996307c933
Simon Green <simon.green@thielsen.co.uk>
2019-02-20 10:10:06 +0000

    Added encode function

--- test_vigenere.py
+++ test_vigenere.py
@@ -4,22 +4,22 @@ from vigenere import combine_character
 def test_encode():
       cipher = VigenereCipher("DONKEY")
       encoded = cipher.encode("TESTENCODING")
-      assert encoded == "Test encode"
+      assert encoded == "WSFDILFCQSRE"
 
 def test_encode_character():
       cipher = VigenereCipher("DONKEY")
       encoded = cipher.encode("E")
-      assert encoded == "X"
+      assert encoded == "H"
 
 def test_encode_spaces():
       cipher = VigenereCipher("DONKEY")
       encoded = cipher.encode("TEST ENCODING")
-      assert encoded == "Test encode"
+      assert encoded == "WSFDILFCQSRE"
 
 def test_encode_mixedcase():
       cipher = VigenereCipher("DONkey")
       encoded = cipher.encode("Test ENCOding")
-      assert encoded == "Test encode"
+      assert encoded == "WSFDILFCQSRE"
 
 def test_combine_character():
       assert combine_character("F", "T") == "Y"
--- vigenere.py
+++ vigenere.py
@@ -1,11 +1,22 @@
 class VigenereCipher:
+
     def __init__(self, keyword):
         self.keyword = keyword
+
     def encode(self, plaintext):
         return "Test encode"
+
     def extend_keyword(self, number):
         repeats = number // len(self.keyword) + 1
         return (self.keyword * repeats)[:number]
+
+    def encode(self, plaintext):
+           plaintext = plaintext.replace(" ", "").upper()
+           cipher = []
+           keyword = self.extend_keyword(len(plaintext))
+           for p,k in zip(plaintext, keyword):
+               cipher.append(combine_character(p,k))
+           return "".join(cipher)    
 
 def combine_character(plain, keyword):
     plain = plain.upper()
