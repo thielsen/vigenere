5a8c1aed667b2460be64edb553af6ead29512d13
Simon Green <simon.green@thielsen.co.uk>
2019-02-20 10:40:15 +0000

    Pylint cleanup

--- test_vigenere.py
+++ test_vigenere.py
@@ -3,39 +3,40 @@ from vigenere import combine_character
 from vigenere import separate_character
 
 def test_encode():
-      cipher = VigenereCipher("DONKEY")
-      encoded = cipher.encode("TESTENCODING")
-      assert encoded == "WSFDILFCQSRE"
+    cipher = VigenereCipher("DONKEY")
+    encoded = cipher.encode("TESTENCODING")
+    assert encoded == "WSFDILFCQSRE"
 
 def test_encode_character():
-      cipher = VigenereCipher("DONKEY")
-      encoded = cipher.encode("E")
-      assert encoded == "H"
+    cipher = VigenereCipher("DONKEY")
+    encoded = cipher.encode("E")
+    assert encoded == "H"
 
 def test_encode_spaces():
-      cipher = VigenereCipher("DONKEY")
-      encoded = cipher.encode("TEST ENCODING")
-      assert encoded == "WSFDILFCQSRE"
+    cipher = VigenereCipher("DONKEY")
+    encoded = cipher.encode("TEST ENCODING")
+    assert encoded == "WSFDILFCQSRE"
 
 def test_encode_mixedcase():
-      cipher = VigenereCipher("DONkey")
-      encoded = cipher.encode("Test ENCOding")
-      assert encoded == "WSFDILFCQSRE"
+    cipher = VigenereCipher("DONkey")
+    encoded = cipher.encode("Test ENCOding")
+    assert encoded == "WSFDILFCQSRE"
 
 def test_combine_character():
-      assert combine_character("F", "T") == "Y"
-      assert combine_character("O", "R") == "F"
+    assert combine_character("F", "T") == "Y"
+    assert combine_character("O", "R") == "F"
 
 def test_extend_keyword():
-      cipher = VigenereCipher("DONKEY")
-      extended = cipher.extend_keyword(20)
-      assert extended == "DONKEYDONKEYDONKEYDO"
+    cipher = VigenereCipher("DONKEY")
+    extended = cipher.extend_keyword(20)
+    assert extended == "DONKEYDONKEYDONKEYDO"
 
 def test_decode_separate_character():
-      assert separate_character("Y", "T") == "F"
-      assert separate_character("F", "R") == "O"
+    assert separate_character("Y", "T") == "F"
+    assert separate_character("F", "R") == "O"
 
 def test_decode():
-      cipher = VigenereCipher("DONKEY") 
-      decoded = cipher.decode("WSFDILFCQSRE")
-      assert decoded == "TESTENCODING"
+    cipher = VigenereCipher("DONKEY")
+    decoded = cipher.decode("WSFDILFCQSRE")
+    assert decoded == "TESTENCODING"
+    
--- vigenere.py
+++ vigenere.py
@@ -1,4 +1,4 @@
-class VigenereCipher:
+class VigenereCipher(object):
 
     def __init__(self, keyword):
         self.keyword = keyword
@@ -9,16 +9,16 @@ class VigenereCipher:
 
     def encode(self, plaintext):
         return self._code(plaintext, combine_character)
-    
+
     def decode(self, ciphertext):
         return self._code(ciphertext, separate_character)
-    
+
     def _code(self, text, combine_func):
         text = text.replace(" ", "").upper()
         combined = []
         keyword = self.extend_keyword(len(text))
-        for p,k in zip(text, keyword):
-            combined.append(combine_func(p,k))
+        for words, k in zip(text, keyword):
+            combined.append(combine_func(words, k))
         return "".join(combined)
 
 def combine_character(plain, keyword):
@@ -28,7 +28,7 @@ def combine_character(plain, keyword):
     keyword_num = ord(keyword) - ord('A')
     return chr(ord('A') + (plain_num + keyword_num) % 26)
 
-def separate_character(cypher, keyword): 
+def separate_character(cypher, keyword):
     cypher = cypher.upper()
     keyword = keyword.upper()
     cypher_num = ord(cypher) - ord('A')
