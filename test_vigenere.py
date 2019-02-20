53fac98ae667c6fae6f6d2f68d137492c607ca6c
Simon Green <simon.green@thielsen.co.uk>
2019-02-20 09:42:38 +0000

    More tests written

--- test_vigenere.py
+++ test_vigenere.py
@@ -4,3 +4,22 @@ def test_encode():
       cipher = VigenereCipher("DONKEY")
       encoded = cipher.encode("TESTENCODING")
       assert encoded == "Test encode"
+
+def test_encode_character():
+      cipher = VigenereCipher("DONKEY")
+      encoded = cipher.encode("E")
+      assert encoded == "X"
+
+def test_encode_spaces():
+      cipher = VigenereCipher("DONKEY")
+      encoded = cipher.encode("TEST ENCODING")
+      assert encoded == "Test encode"
+
+def test_encode_mixedcase():
+      cipher = VigenereCipher("DONkey")
+      encoded = cipher.encode("Test ENCOding")
+      assert encoded == "Test encode"
+
+def test_combine_character():
+      assert combine_character("F", "T") == "Y"
+      assert combine_character("O", "R") == "F"
